57cd8770ebc4651550d44e981943fd6164e4d07e
morchus13 <nayasebastian84@gmail.com>
2023-11-03 22:47:08 -0300

    TP-javascrip v2.7

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 
-/* Deberá contener la funcionalidad en Javascript, al momento de 
-presionar el botón “Resumen”, deberá mostrar en la sección 
-“Total a Pagar: $”, el monto correspondiente a la cantidad de 
-tickets a comprar con el descuento correspondiente dependiendo la 
-categoría seleccionada, existen 3 categorías, Estudiante, Trainee,
- Junior*/
+//Selecciono los elementos HTML que necesito para manipular.-
 
 const categoria = document.getElementById("categoria");
 const totalPagar = document.getElementById("totalPagar");
 const cantTicket = document.querySelector('#cantidad');
-
 const formulario = document.getElementById("formulario");
 
+
+//Cuando se dispara el evento del formulario lo detengo, lo valido
 formulario.addEventListener("submit", function(e){
-	console.log(e);
 	e.preventDefault();
-	//aca creo que deberia validar antes de mandar todo al servidor.-
 	let error=validar();
 	if(error[0]){
 		alert(error[1]);
@@ -23,35 +17,27 @@ formulario.addEventListener("submit", function(e){
 		alert("Gracias!!! Se enviarán sus datos ingresados...");
 		limpiar();
 	}
-
-	
-	
 });
 
-
+//Si el ususario selecciona una categoria automaticamnete y si coloco una cantidad realizo el calculo y lo muestro
 categoria.addEventListener("change", function(e){
 	
 	// Defino las varables internas
 	const valor= 200;
 	const cat=categoria.value;
 	let cant=cantTicket.value;
-	// Evaluo el valor de cantidad si es vacio le asigno 1
-	if(cant=='') {
-		cant=1;
-		//podria poner un alert q avise que no hay cantidad seleccionada.-
-	}
 
 	// Variables para calcular los descuentos y los pongo en 0
 	let descEstudiante=0;
 	let descTrainer=0;
 	let descJunior =0;
 
-	// calculo cada descuento
+	// calculo cada descuento segun la categoria
 	descEstudiante= (valor - valor * 0.8) * cant;
 	descTrainer=(valor - valor * 0.5) * cant;
 	descJunior= (valor - valor * 0.15) * cant;
 	
-	// Depoendiendo que elige el usuario agrego el descuento a la etiqueta
+	//Agrego al lbl de total a pagar el texto con el valor de los calculos anteriores
 	if (categoria.value == "estudiante"){
 		totalPagar.innerText=`Total a Pagar: $${descEstudiante}`;
 	} else if( categoria.value == "trainer"){
@@ -61,28 +47,24 @@ categoria.addEventListener("change", function(e){
 	}
 });
 
-// mismo evento por si el usuario cambia la cantidad se actualiza el precio
+//Si el ususario coloca una cantidad automaticamnete y si ya selecciono una categoria hago el calculo y muestro.-
 cantTicket.addEventListener("change", function(){
-	// Defino las varables internas
+
 	const valor= 200;
 	const cat=categoria.value;
 	let cant=cantTicket.value;
-	// Evaluo el valor de cantidad si es vacio le asigno 1
-	if(cant=='') {
-		cant=1;
-	}
-
+	
 	// Variables para calcular los descuentos y los pongo en 0
 	let descEstudiante=0;
 	let descTrainer=0;
 	let descJunior =0;
 
-	// calculo cada descuento
+	// calculo cada descuento segun la categoria
 	descEstudiante= (valor - valor * 0.8) * cant;
 	descTrainer=(valor - valor * 0.5) * cant;
 	descJunior= (valor - valor * 0.15) * cant;
 	
-	// Depoendiendo que elige el usuario agrego el descuento a la etiqueta
+	//Agrego al lbl de total a pagar el texto con el valor de los calculos anteriores
 	if (categoria.value == "estudiante"){
 		totalPagar.innerText=`Total a Pagar: $${descEstudiante}`;
 	} else if( categoria.value == "trainer"){
@@ -94,7 +76,6 @@ cantTicket.addEventListener("change", function(){
 
 
 // BOTON BORRAR
-
 const borrar = document.getElementById("borrar");
 borrar.addEventListener("click", limpiar);
 
@@ -105,7 +86,7 @@ function limpiar(){
 	totalPagar.innerText=`Total a Pagar:`;	
 }
 
-//validaciond de campos
+//VALIDACION de campos Nombre Apellido y de el email.-
 
 const nombre = document.getElementById("nombre");
 const apellido = document.getElementById("apellido");
@@ -130,7 +111,6 @@ function validar(){
 		error[1]='Debe ingresar una categoria de ticket';
 		return error;
 	}
-
 	error[0]=false;
 	return error;
 }
